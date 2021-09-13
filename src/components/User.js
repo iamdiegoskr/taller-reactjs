550bdda304b7d033152708ab928f82f49b46091c
Diego Martinez <juandiego337@hotmail.com>
2021-09-13 17:39:48 -0500

    Refactoring the form to implement hook forms

--- src/components/User.js
+++ src/components/User.js
@@ -1,73 +1,82 @@
 import React,{useState} from 'react'
 import TableUsers from './TableUsers'
 import '../styles/User.css'
+import { useForm } from "react-hook-form";
 
 function UserForm() {
 
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
     const [listUsers, setlistUsers] = useState([])
 
-    const [users, setUsers] = useState({
-        nombre: '',
-        edad: '',
-        ocupacion: 'estudiante',
-        descripcion: ''
-    })
+    const onSubmit = (user, event) =>{
 
-    const handleInputChange = (event) => {
-        setUsers({
-            ...users,
-            [event.target.name] : event.target.value
-        })
-    }
-
-    const enviarDatos = (event) => {
-        event.preventDefault()
-
-        let description = '';
-
-        if(users.edad>=0 && users.edad<=12){
-            description = `Al niñ@ ${users.nombre} de ${users.edad} años,
-            le recomendamos disfrutar de su niñez,aprender y respetar`;
-        }else if(users.edad<=30){
-            description = `Al Joven ${users.nombre} de ${users.edad} años,
-                le recomendamos tener presente el compromiso, el esfuerzo y el respeto como principales valores
-                para obtener un buen resultado como ${users.ocupacion}`;
-        }else if(users.edad<=50){
-            description = `Al Adulto ${users.nombre} de ${users.edad} años,
-                le recomendamos tener presente la paciencia como ${users.ocupacion}`;
-        }else{
-            description = `Al Mayor ${users.nombre} de ${users.edad} años,
-                le recomendamos disfrutar sus ultimos dias`;
+        let newUser = {
+            nombre : user.nombre,
+            edad : user.edad,
+            ocupacion : user.ocupacion,
+            descripcion : messageByAge(user)
         }
 
-        users.descripcion = description;
+        setlistUsers(listUsers => [...listUsers, newUser]);
 
-        setlistUsers(listUsers => [...listUsers, users]);
+        event.target.reset()
 
-        clear(event.target)
     }
 
-    function clear(event){
-        event.reset();
+    const messageByAge = (user)=>{
+
+        let {edad, nombre, ocupacion} = user;
+
+        if(edad>=0 && edad<=12){
+            return `Al niñ@ ${nombre} de ${edad} años,
+                le recomendamos disfrutar de su niñez,aprender y respetar`;
+        }else if(edad<=30){
+            return `Al Joven ${nombre} de ${edad} años,
+                le recomendamos tener presente el compromiso, el esfuerzo y el respeto como principales valores
+                para obtener un buen resultado como ${ocupacion}`
+        }else if(edad<=50){
+            return `Al Adulto ${nombre} de ${edad} años,
+            le recomendamos tener presente la paciencia como ${ocupacion}`
+        }else{
+            return `Al Mayor ${nombre} de ${edad} años,
+            le recomendamos disfrutar sus ultimos dias`
+        }
     }
+
 
     return (
         <div>
 
-            <form className="user-section"  onSubmit={enviarDatos}>
-                <input type="text" className="user" placeholder="Ingrese su nombre"
-                        onChange={handleInputChange} name="nombre" required /> <br/>
+            <form className="user-section" onSubmit={handleSubmit(onSubmit)}>
 
-                <input type="number" min="0" max="200" className="user-age" placeholder="Ingrese su edad"
-                        onChange={handleInputChange} name="edad" required /><br/>
+                <input  type="text"
+                        className="user"
+                        placeholder="Ingrese su nombre"
+                        {...register("nombre", {
+                            required: true,
+                            maxLength: 30,
+                            pattern: /^[A-Za-z]+$/i
+                        })}
+                /><br/>
+                {errors?.nombre?.type === "required" && <p className="error">El nombre es obligatorio</p>}
 
-                <select name="ocupacion" id="ocupacion" onChange={handleInputChange}>
+                <input
+                    type="number"
+                    className="user-age"
+                    placeholder="Ingrese su edad"
+                    {...register("edad", { required: true ,min: 0, max: 99 })} /><br/>
+                {errors.edad && (
+                    <p className="error">Ingresa una edad valida</p>
+                )}
+
+                <select name="ocupacion" {...register("ocupacion")} >
                     <option value="estudiante">Estudiante</option>
                     <option value="empleado">Empleado</option>
                     <option value="jubilado">Jubilado</option>
                 </select><br/>
 
-                <button type="submit" className="btn-register" >Registrarse</button>
+                <input type="submit" className="btn-register" />
             </form>
 
             <hr/>
